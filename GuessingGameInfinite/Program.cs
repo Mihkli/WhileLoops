410fa36d75f21384f4b2e029daab1e3fbc781b6b
Opilane TTHK <opilane@tthk.ee>
2021-09-28 11:56:54 +0300

    first commit

---
+++ GuessingGameInfinite/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GuessingGameInfinite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int cpurandom = rnd.Next(1, 11);
+
+            while (true)
+            {
+                Console.WriteLine("Sisesta number");
+                int userguess = Convert.ToInt32(Console.ReadLine());
+
+                if(userguess == cpurandom)
+                {
+                    Console.WriteLine("Tubli! Oled mängu võitnud");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Proovi uuesti");
+                }
+            }
+            
+            
+        }
+    }
+}
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /*while (true) 
+            {
+                Console.WriteLine("Sisesta PIN-kood:");
+                int userPIn = Convert.ToInt32(Console.ReadLine());
+
+                if (userPIn == 1234)
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Vale PIN proovi uuesti");
+                }
+            }
+            Console.WriteLine("Kena päeva");*/
+            int i = 0;
+            while(i < 3)
+            {
+                Console.WriteLine("Sisesta PIN-kood");
+                int userpin = Convert.ToInt32(Console.ReadLine());
+                if(userpin == 1234)
+                {
+                    Console.WriteLine("Tere tulemast");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Vale pin {3 - i} katset on jäänud.");
+                    i = i + 1;
+
+                }
+            }
+            Console.WriteLine("Kena päeva!");
+            
+            
+
+        }
+    }
+}
